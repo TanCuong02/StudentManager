28ad22c4f6143629c45a637f5003af629dcc8db4
TanCuong1312 <97434754+TanCuong13122002@users.noreply.github.com>
2024-11-16 15:05:38 +0700

    Add

---
+++ StudentManager/src/Main.java
@@ -0,0 +1,8 @@
+import Service.StudentManager;
+
+public class Main {
+    public static void main(String[] args) {
+        StudentManager s = new StudentManager();
+        s.start();
+    }
+}
---
+++ StudentManager/src/Service/StudentManager.java
@@ -0,0 +1,7 @@
+package Service;
+
+public class StudentManager {
+    public void start(){
+        
+    }
+}
